de85a4bc3d3a3fa18905f32e6106891d29c89414
qudtjs0753 <qudtjs0753@naver.com>
2022-02-10 22:55:04 +0900

    Add - spring initializr로 spring 의존성 세팅

---
+++ src/main/java/com/codrawer/codrawer_back_end/CodrawerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.codrawer.codrawer_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodrawerBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodrawerBackEndApplication.class, args);
+    }
+
+}
